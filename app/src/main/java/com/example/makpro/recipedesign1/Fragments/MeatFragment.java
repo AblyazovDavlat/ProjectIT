242c6e04a11952df20c00489a6ce29bce03e5a41
Ablyazov Davlat <ablyazovdavlat@gmail.com>
2016-04-18 23:02:29 +0300

    Commit 2.0

--- app/src/main/java/com/example/makpro/recipedesign1/Fragments/MeatFragment.java
+++ app/src/main/java/com/example/makpro/recipedesign1/Fragments/MeatFragment.java
@@ -94,39 +94,39 @@ public class MeatFragment extends Fragment implements View.OnClickListener {
             public void onClick(View v)
            {
                if (chicken.isChecked()) {
-                   staticString.str.remove("chicken");
-                   staticString.str.add("chicken");
+                   staticString.str.remove("Comp_Ingredient_ID = 135");
+                   staticString.str.add("Comp_Ingredient_ID = 135");
                }
                else
-                   staticString.str.remove("chicken");
+                   staticString.str.remove("Comp_Ingredient_ID = 135");
 
                if (Pig.isChecked()) {
-                   staticString.str.remove("pig");
-                   staticString.str.add("pig");
+                   staticString.str.remove("Comp_Ingredient_ID = 134");
+                   staticString.str.add("Comp_Ingredient_ID = 134");
                }
                else
-                   staticString.str.remove("pig");
+                   staticString.str.remove("Comp_Ingredient_ID = 134");
 
                if (turkey.isChecked()) {
-                   staticString.str.remove("turkey");
-                   staticString.str.add("turkey");
+                   staticString.str.remove("Comp_Ingredient_ID = 136");
+                   staticString.str.add("Comp_Ingredient_ID = 136");
                }
                else
-                   staticString.str.remove("turkey");
+                   staticString.str.remove("Comp_Ingredient_ID = 136");
 
                if (cow.isChecked()) {
-                   staticString.str.remove("cow");
-                   staticString.str.add("cow");
+                   staticString.str.remove("Comp_Ingredient_ID = 133");
+                   staticString.str.add("Comp_Ingredient_ID = 133");
                }
                else
-                   staticString.str.remove("cow");
+                   staticString.str.remove("Comp_Ingredient_ID = 133");
 
                if (rabbit.isChecked()) {
-                   staticString.str.remove("rabbit");
-                   staticString.str.add("rabbit");
+                   staticString.str.remove("Comp_Ingredient_ID = 133");
+                   staticString.str.add("Comp_Ingredient_ID = 133");
                }
                else
-                   staticString.str.remove("rabbit");
+                   staticString.str.remove("Comp_Ingredient_ID = 133");
 
                if (bear.isChecked()) {
                    staticString.str.remove("bear");
@@ -182,138 +182,5 @@ public class MeatFragment extends Fragment implements View.OnClickListener {
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
-    }
-
-    /**
-     * Created by Давлат on 16.04.2016.
-     */
-    public static class DBHelper extends SQLiteOpenHelper {
-
-        //для работы с ресурсами
-        private final Context fContext;
-        ContentValues contentValues;
-        Resources res;
-
-        public DBHelper(Context context) {
-            super(context, "CookDB", null, 1);
-            fContext = context;
-        }
-
-        public void onCreate(SQLiteDatabase db) {
-
-            contentValues = new ContentValues();
-            res = fContext.getResources();
-
-            //СОЗДАНИЕ ТАБЛИЦ БАЗЫ ДАННЫХ----------------------------------------------------------
-
-            //создаем таблицу !РЕЦЕПТЫ!---------------
-            db.execSQL("create table Recipe ("
-                    +"Recipe_ID integer primary key,"
-                    +"Rec_Cuisine_ID integer,"
-                    +"Rec_Category_ID integer,"
-                    +"Rec_Cooking_method_ID integer,"
-                    +"Description_cooking_method text,"
-                    +"Recipe_name text,"
-                    +"Caloric_content text,"
-                    +"Rec_Author_ID integer," + ");");
-
-            //создаем таблицу !КУХНЯ!------------------
-            db.execSQL("create table Cuisine("
-                    + "Cuisine_ID integer primary key,"
-                    + "Cuisine_name text," + ");");
-
-
-            //создаем таблицу !ВИД БЛЮДА!---------------
-            db.execSQL("create table Category("
-                    + "Category_ID integer primary key,"
-                    + "Category_name text," + ");");
-
-            //создаем таблицу !СПОСОБ ПРИГТОВЛЕНИЯ!-----------
-            db.execSQL("create table Cooking_method ("
-                    + "Cooking_method_ID integer primary key,"
-                    + "Method_name text," + ");");
-
-            //создаем таблицу !СОСТАВ БЛЮДА!--------
-            db.execSQL("create table Category ("
-                    + "Comp_ID integer primary key,"
-                    + "Comp_Ingredient_ID integer,"
-                    + "Comp_recipe_ID integer,"
-                    + "Comp_Unit_measure_ID integer,"
-                    + "Quantity integer," + ");");
-
-            //создаем таблицу !ВРЕМЯ!---------------
-            db.execSQL("create table Time ("
-                    +"Time_ID integer primary key,"
-                    +"Time_name text," + ");");
-
-
-            //создаем таблицу !ЕДИНИЦЫ ИЗМЕРЕНИЯ!----------
-            db.execSQL("create table Unit_measure ("
-                    +"Unit_measure_ID integer primary key,"
-                    +"Unit_measure_name text," + ");");
-
-            //создаем таблицу !ИНГРЕДИЕНТ!---------------
-            db.execSQL("create table Ingredient ("
-                    +"Ingredient_ID integer primary key,"
-                    +"Ingredient_name text," + ");");
-
-            //создаем таблицу !ОТЗЫВ!-------------------
-            db.execSQL("create table Reference ("
-                    +"Reference_ID integer primary key,"
-                    +"Ref_Recipe_ID integer,"
-                    +"Message text,"
-                    +"Date datetime,"
-                    + ");");
-
-            //ЗАПОЛНЕНИЕ ТАБЛИЦ ИЗ РЕСУРСОВ------------------------------------------------------------
-
-            //заполнение таблицы !КУХНЯ!
-
-            String[] cuisine_name = res.getStringArray(R.array.Cuisine_name);
-            int[] cuisine_id = res.getIntArray(R.array.Cuisine_id);
-
-            for (int i=0; i<cuisine_id.length;i++){
-                contentValues.clear();
-                contentValues.put("Cuisine_ID",cuisine_id[i]);
-                contentValues.put("Cuisine_name",cuisine_name[i]);
-                db.insert("Cuisine",null,contentValues);
-            }
-
-            //заполнение таблицы !ВИД БЛЮДА!
-
-            String[] category_name = res.getStringArray(R.array.Category_name);
-            int[] category_id = res.getIntArray(R.array.Category_id);
-
-            for (int i=0; i<category_id.length;i++){
-                contentValues.clear();
-                contentValues.put("Category_ID",category_id[i]);
-                contentValues.put("Category_name",category_name[i]);
-                db.insert("Category",null,contentValues);
-            }
-
-            //заполнение таблицы !СПОСОБ ПРИГТОВЛЕНИ!
-
-            String[] method_name = res.getStringArray(R.array.Method_name);
-            int[]  cooking_method_id = res.getIntArray(R.array.Cooking_method_ID);
-
-            for (int i=0; i<cooking_method_id.length;i++){
-                contentValues.clear();
-                contentValues.put("Cooking_method_ID",cooking_method_id[i]);
-                contentValues.put("Method_name",method_name[i]);
-                db.insert("Cooking_method",null,contentValues);
-            }
-
-
-
-
-
-
-
-
-        }
-
-        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
-        }
     }
 }
